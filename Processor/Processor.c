200847d2aad8fa9b4b56fec5e1d45106ec0a7e9c
Krishna Teja Ayinala <teja.ayinala@gmail.com>
2019-05-28 02:59:42 -0500

    Created a processor folder - to be implemented (i/o operations)

---
+++ Processor/Processor.c
@@ -0,0 +1,6 @@
+//
+// Created by Krishna Teja Ayinala & Sindhura Bonthu on 2019-05-28.
+//
+
+// a)Read input file
+// b)Queue instructions
---
+++ Processor/Processor.h
@@ -0,0 +1,8 @@
+//
+// Created by Krishna Teja Ayinala & Sindhura Bonthu on 2019-05-28.
+//
+
+#ifndef ACA_TERMPROJECT_PROCESSOR_H
+#define ACA_TERMPROJECT_PROCESSOR_H
+
+#endif //ACA_TERMPROJECT_PROCESSOR_H
