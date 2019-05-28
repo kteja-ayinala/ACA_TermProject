bf7d724abeaab16aebab622f263c33a33ce5210f
Sindhura <sbonthu@unomaha.edu>
2019-05-28 15:01:26 -0500

    Merge branch 'master' of /Users/sindhurabonthu/CLionProjects/ACA_TermProject with conflicts.

--- Processor/Processor.c
+++ Processor/Processor.c
@@ -1,6 +1,35 @@
 //
 // Created by Krishna Teja Ayinala & Sindhura Bonthu on 2019-05-28.
 //
+#include<Processor.h>
+#include<stdio.h>
+
+void run_processor(Processor* processor)
+{
+    FILE * f = fopen("input.txt", "r"); //take input file
+    while(!feof(f))
+    {
+        if(strcmp(instruction, "CPURead")) //check if the instruction is Read instruction
+        {
+            fscanf(f, "%d", &address);
+            rw = 0;
+            char* addressBitString = convertAddressToBinary(address);
+            //
+        }
+        else if(strcmp(instruction, "CPUWrite")) // check if the instruction is Write
+        {
+            fscanf(f, "%d %[^\n]", &address, &value);
+            rw = 1;
+            char* addressBitString = convertAddressToBinary(address);
+            //
+        }
+
+        if(rw==0)
+        {
+
+        }
+    }
+}
 
 // a)Read input file
 // b)Queue instructions
--- Processor/Processor.h
+++ Processor/Processor.h
@@ -6,11 +6,14 @@
 #define ACA_TERMPROJECT_PROCESSOR_H
 
 typedef struct Processor{
-    unsigned valid: 1;
-    unsigned address: 18;
-    unsigned char data[32];
-    unsigned rw: 1;
+    unsigned address: 18; // CPU address
+    unsigned rw: 1; //0 for CPU read and 1 for write
+    char instruction[100]; //CPU instruction
+    char value; // for write operation
 };
 
+extern struct Processor;
+
+void run_processor(Processor* processor);
 
 #endif //ACA_TERMPROJECT_PROCESSOR_H
